b42cc4e55c4f367cb01ae46ae0da9037de5fbd0f
Abhishek kr. Pandit <akp167585@gmail.com>
2025-04-30 18:17:24 +0530

    1st

---
+++ DVN_SCHOOL_PROJECT/projectapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+# class signupdata(models.Model):
+#     email=models.CharField(max_length=50)
+#     password=models.CharField(max_length=20)
